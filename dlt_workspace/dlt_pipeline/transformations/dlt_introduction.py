b91daa5bac0c5846f696b0d2a017b4a9f58cd967
Kunalthebug <iamkunaldas333@gmail.com>
2026-02-24 16:08:51 +0000

    First Hands on Delta Live Tables Using ETL pipeline

---
+++ dlt_workspace/dlt_pipeline/transformations/dlt_introduction.py
@@ -0,0 +1,56 @@
+import dlt
+from pyspark.sql.functions import *
+
+#Creating orders streaming bronze table
+@dlt.table(
+    table_properties={"quality": "bronze"},
+    comment = "orders bronze table"
+)
+
+def orders_bronze():
+    df = spark.readStream.table("dev.bronze.orders_raw")
+    return df
+
+#Creating customer materialized view
+@dlt.table(
+    table_properties={"quality": "bronze"},
+    comment = "customers bronze table",
+    name = "customer_bronze"
+)
+
+def customer_bronze():
+    df = spark.read.table("dev.bronze.customers_raw")
+    return df
+
+#creating view to join orders table and customer table
+@dlt.view(
+    comment = "Joined table"
+)
+
+def joined_vw():
+    df_c = spark.read.table("LIVE.customer_bronze")
+    df_o = spark.read.table("LIVE.orders_bronze")
+    df = df_o.join(df_c,how="left_outer", on=df_o.o_custkey == df_c.c_custkey)
+    return df
+
+#create MV to add a new column
+@dlt.table(
+    table_properties={"quality": "silver"},
+    comment = "joined silver table",
+    name = "joined_silver"
+)
+
+def joined_silver():
+    df = spark.read.table("LIVE.joined_vw").withColumn("__insert_date",current_timestamp())
+    return df
+
+#Aggregate based on c_mktsegment and find the count of order (c_orderkey)
+@dlt.table(
+    table_properties = {"quality": "gold"},
+    comment = "orders aggregated table"
+)
+
+def orders_agg_gold():
+    df = spark.read.table("LIVE.joined_silver")
+    df_final = df.groupBy("c_mktsegment").agg(count("o_orderkey").alias("sum_orders"))
+    return df_final
