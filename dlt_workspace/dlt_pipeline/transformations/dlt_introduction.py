672c274aa21b56f913e1854d5e1e574d0d34e4df
Kunalthebug <iamkunaldas333@gmail.com>
2026-02-24 19:08:52 +0000

    Second commit on DLT
    ...introduced conditional MV creation at gold layer & Autoloader functionality at one source table (orders)

--- dlt_workspace/dlt_pipeline/transformations/dlt_introduction.py
+++ dlt_workspace/dlt_pipeline/transformations/dlt_introduction.py
@@ -1,6 +1,8 @@
 import dlt
 from pyspark.sql.functions import *
 
+#Reading the parameters
+_order_status = spark.conf.get("custom.orderStatus","NA")
 #Creating orders streaming bronze table
 @dlt.table(
     table_properties={"quality": "bronze"},
@@ -9,6 +11,44 @@ from pyspark.sql.functions import *
 
 def orders_bronze():
     df = spark.readStream.table("dev.bronze.orders_raw")
+    return df
+
+#Creating orders streaming bronze table sourced using AUTOLOADER
+@dlt.table(
+    table_properties={"quality": "bronze"},
+    comment = "orders Autoloader",
+    name = "orders_autoloader_bronze"
+)
+
+def func():
+    df = (
+        spark.readStream.format("cloudFiles")
+        .option("cloudFiles.schemaHints","o_orderkey bigint ,o_custkey bigint,o_orderstatus string,o_totalprice decimal(18,2),o_orderdate date,o_orderpriority string ,o_clerk string,o_shippriority int,o_comment string")
+        .option("cloudFiles.schemaLocation","/Volumes/dev/bronze/landing/autoloader/schemas/")
+        .option("cloudFiles.format","CSV")
+        .option("pathGlobFilter","*.csv")
+        .option("cloudFiles.schemaEvolutionMode","none")
+        .load("/Volumes/dev/bronze/landing/files/")
+    )
+    return df
+
+#Create blank Streaming Tables 
+dlt.create_streaming_table("orders_union_bronze")
+
+#Append Flow
+@dlt.append_flow(
+    target = "orders_union_bronze"
+)
+def order_delta_append():
+    df = spark.readStream.table("LIVE.orders_bronze")
+    return df
+
+#Append Flow
+@dlt.append_flow(
+    target = "orders_union_bronze"
+)
+def order_autoloader_append():
+    df = spark.readStream.table("LIVE.orders_autoloader_bronze")
     return df
 
 #Creating customer materialized view
@@ -29,7 +69,7 @@ def customer_bronze():
 
 def joined_vw():
     df_c = spark.read.table("LIVE.customer_bronze")
-    df_o = spark.read.table("LIVE.orders_bronze")
+    df_o = spark.read.table("LIVE.orders_union_bronze")
     df = df_o.join(df_c,how="left_outer", on=df_o.o_custkey == df_c.c_custkey)
     return df
 
@@ -52,5 +92,17 @@ def joined_silver():
 
 def orders_agg_gold():
     df = spark.read.table("LIVE.joined_silver")
-    df_final = df.groupBy("c_mktsegment").agg(count("o_orderkey").alias("sum_orders"))
+    df_final = df.groupBy("c_mktsegment").agg(count("o_orderkey").alias("count_orders"))
     return df_final
+
+for _status in _order_status.split(","):
+    @dlt.table(
+    table_properties = {"quality": "gold"},
+    comment = "orders aggregated table",
+    name = f"orders_agg_{_status}_gold"
+    )
+
+    def func():
+        df = spark.read.table("LIVE.joined_silver")
+        df_final = df.where(f"o_orderstatus = '{_status}'").groupBy("c_mktsegment").agg(count("o_orderkey").alias("count_orders"))
+        return df_final
